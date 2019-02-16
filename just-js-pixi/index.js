420cb14ebfe156bade9f0528db9dd8809b07da77
tjabejohannes <tjabejv@stud.ntnu.no>
2019-02-16 15:33:51 +0100

    Adding drag functionalety 300,                          //Total duration, in frames

--- just-js-pixi/index.js
+++ just-js-pixi/index.js
@@ -1,4 +1,4 @@
-const log = console.log();
+const log = console.log;
 const app = new PIXI.Application({
     width: screen.availWidth,         // default: 800
     height: screen.availHeight - 130,        // default: 600
@@ -18,28 +18,60 @@ let truck = new PIXI.Sprite(new PIXI.Texture(texture));
 truck.interactive = true;
 truck.buttonMode = true;
 truck.anchor.set(0.5);
-truck.x = 500;
-truck.y = 500;
+truck.x = 300;
+truck.y = 300;
+
+/*let curve = [
+    [truck.x, truck.y],   //Start position
+    [truck.x+100, truck.y+100],                   //Control point 1
+    [truck.x-100, truck.y-100],                    //Control point 2
+    [truck.x, truck.y],                     //End position
+];*/
 
 let curve = [
-    [truck.x, truck.y],   //Start position
-    [108, 32],                    //Control point 1
-    [176, 32],                    //Control point 2
-    [196, 160]                    //End position
+    //First curve
+    [[truck.x, truck.y], [truck.x + 75, truck.y + 500], [truck.x + 200, truck.y + 500], [truck.x + 300, truck.y + 300]],
+
+    //Second curve
+    [[truck.x + 300, truck.y + 300], [truck.x + 250, truck.y + 100], [truck.x + 100, truck.y + 100], [truck.x, truck.y]]
 ];
 
 
-c.followCurve(
+c.walkCurve(
     truck,            //The sprite
     curve,                //The Bezier curve array
-    120,                  //Duration, in milliseconds
-    "smoothstep",         //Easing type
-    true,                 //Should the tween yoyo?
-    1000                  //Delay, in milliseconds, before it yoyos
+    300,                          //Total duration, in frames
+    "smoothstep",                 //Easing type
+    true,                         //Should the path loop?
+    true,                         //Should the path yoyo?
+    1000                          //Delay in milliseconds between segments
 );
 
 
 app.stage.addChild(truck);
+
+const onDragStart = event => {
+    truck.data = event.data;
+    truck.dragging = true;
+};
+
+const onDragEnd = event => {
+    delete truck.data;
+    truck.dragging = false;
+};
+
+const onDragMove = event => {
+    if (truck.dragging === true) {
+        const newPosition = truck.data.getLocalPosition(truck.parent);
+        truck.x = newPosition.x;
+        truck.y = newPosition.y;
+    };
+};
+
+truck.on('pointerdown', onDragStart)
+    .on('pointerup', onDragEnd)
+    .on('pointerupoutside', onDragEnd)
+    .on('pointermove', onDragMove)
 
 
 function gameLoop() {
